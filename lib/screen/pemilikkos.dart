17603e100b3d5f042be97c0b1aa66c7990deb196
rafirhnnn <rafirhn17@gmail.com>
2025-01-15 10:19:29 +0700

    update navibar

--- lib/screen/pemilikkos.dart
+++ lib/screen/pemilikkos.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 
-class PemilikKosScreen extends StatelessWidget {
+class PemilikKosScreen extends StatefulWidget {
   const PemilikKosScreen({super.key});
+
+  @override
+  State<PemilikKosScreen> createState() => _PemilikKosScreenState();
+}
+
+class _PemilikKosScreenState extends State<PemilikKosScreen> {
+  int _selectedIndex = 0;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +30,55 @@ class PemilikKosScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Pemilik Kos'),
       ),
-      body: const Center(
-        child: Text('Welcome to Pemilik Kos Dashboard!'),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        children: const [
+          Center(child: Text('Home Page')),
+          Center(child: Text('Users Page')),
+          Center(child: Text('Messages Page')),
+          Center(child: Text('Settings Page')),
+        ],
+      ),
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: _selectedIndex,
+        showElevation: true,
+        onItemSelected: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+          _pageController.animateToPage(
+            index,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.ease,
+          );
+        },
+        items: [
+          BottomNavyBarItem(
+            icon: const Icon(Icons.apps),
+            title: const Text('Home'),
+            activeColor: Colors.red,
+          ),
+          BottomNavyBarItem(
+            icon: const Icon(Icons.people),
+            title: const Text('Users'),
+            activeColor: Colors.purpleAccent,
+          ),
+          BottomNavyBarItem(
+            icon: const Icon(Icons.message),
+            title: const Text('Messages'),
+            activeColor: Colors.pink,
+          ),
+          BottomNavyBarItem(
+            icon: const Icon(Icons.settings),
+            title: const Text('Settings'),
+            activeColor: Colors.blue,
+          ),
+        ],
       ),
     );
   }
--- lib/screen/pencarikos.dart
+++ lib/screen/pencarikos.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 
-class PencariKosScreen extends StatelessWidget {
+class PencariKosScreen extends StatefulWidget {
   const PencariKosScreen({super.key});
+
+  @override
+  State<PencariKosScreen> createState() => _PencariKosScreenState();
+}
+
+class _PencariKosScreenState extends State<PencariKosScreen> {
+  int _selectedIndex = 0;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +30,55 @@ class PencariKosScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Pencari Kos'),
       ),
-      body: const Center(
-        child: Text('Welcome to Pencari Kos Dashboard!'),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        children: const [
+          Center(child: Text('Home Page')),
+          Center(child: Text('Favorites Page')),
+          Center(child: Text('Messages Page')),
+          Center(child: Text('Profile Page')),
+        ],
+      ),
+      bottomNavigationBar: BottomNavyBar(
+        selectedIndex: _selectedIndex,
+        showElevation: true,
+        onItemSelected: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+          _pageController.animateToPage(
+            index,
+            duration: const Duration(milliseconds: 300),
+            curve: Curves.ease,
+          );
+        },
+        items: [
+          BottomNavyBarItem(
+            icon: const Icon(Icons.home),
+            title: const Text('Home'),
+            activeColor: Colors.red,
+          ),
+          BottomNavyBarItem(
+            icon: const Icon(Icons.favorite),
+            title: const Text('Favorites'),
+            activeColor: Colors.purpleAccent,
+          ),
+          BottomNavyBarItem(
+            icon: const Icon(Icons.message),
+            title: const Text('Messages'),
+            activeColor: Colors.pink,
+          ),
+          BottomNavyBarItem(
+            icon: const Icon(Icons.person),
+            title: const Text('Profile'),
+            activeColor: Colors.blue,
+          ),
+        ],
       ),
     );
   }
